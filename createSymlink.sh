a1edb888bd78b9e2f87599af610ada8faa607293
Zeng Dai <daizeng1984@gmail.com>
2016-01-30 21:36:35 -0800

    Added symlink bash shell Added installCentos7 shell Fixed some comments

---
+++ createSymlink.sh
@@ -0,0 +1,33 @@
+# some code is from https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh with slightly modification
+dir=~/.dotfiles                    # dotfiles directory
+olddir=~/.dotfiles_old             # old dotfiles backup directory
+folders="bashscript zshscript vim74lua"    # list of folders that have dot files
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+[ ! -d $olddir ] && mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for folder in $folders; do
+    if [ -d $folder ] ; then
+        cd $dir/$folder
+        echo "$(find -maxdepth 1 -path './*' -prune)"
+        for d in $(find -maxdepth 1 -path './*' -prune) ; do
+            if [ -L ~/test/$d ] ; then
+                rm ~/test/$d
+            else
+                mv ~/test/$d ~/dotfiles_old/
+            fi
+
+            echo "Creating symlink for $d"
+            ln -s $dir/$folder/$d ~/test/$d
+        done
+        cd $dir
+    fi
+done
---
+++ installCentos7.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+# Install on minimal setup
+yum groupinstall "X Window System"
+yum groupinstall "GNOME Desktop"
+
+# 3rd Party Repo 
+# Install Elrepo based on official website, this might be changed
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+# So we could install kmod-nvidia after that
+# yum install kmod-nvidia
+# Install EPEL
+yum install epel-release
+# Install NUX desktop if you need app like audacious
+yum -y install epel-release && rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+
+# Install some tools essential for myself
+yum install gvim
+yum install ntfs-3g
+yum install gcc gcc-c++ git
+yum install tree
+yum install wget
+
+# install chrome
+cd && mkdir Downloads && cd Downloads
+chown -R $USER:$USER
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+yum --nogpgcheck localinstall google-chrome-stable_current_x86_64.rpm
+cd
+
+#install dropbox better from official website's RPM
+#yum install nautilus-dropbox
+
+#Forget about MS Office
+#sh -c "printf '[playonlinux]\nname=PlayOnLinux Official repository\nbaseurl=http://rpm.playonlinux.com/fedora/yum/base\nenable=1\ngpgcheck=0\ngpgkey=http://rpm.playonlinux.com/public.gpg\n' > /etc/yum.repos.d/playonlinux.repo"
+#yum install playonlinux
+
+# autocomplete ignore case
+echo "set completion-ignore-case On" >> $HOME/.inputrc
+
+# Install zsh to be default
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# ZSH should do this by default
+# chsh -s /usr/bin/zsh
+
+# Download dotfiles
+[ -d "$DOTFILES" ] && git clone https://github.com/daizeng1984/configuration.git $DOTFILES
+# Create symlink
+
+
+# Reminde of removing alternateTab in gnome's /usr/share/gnome-shell/extensions
+echo "don't forget to remove alternateTab in /usr/share/gnome-shell/extensions\n"
+
+#reboot
