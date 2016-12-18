47298e88cd99f006bc30fe24a9763aad26fce13c
daizeng1984 <daizeng1984@gmail.com>
2016-12-18 13:40:55 +0800

    - Fixed a bug that mkdir generate ~ folder

--- neovim/installNeoVim.sh
+++ neovim/installNeoVim.sh
@@ -1,10 +1,11 @@
 # Find .config
-[ -z $XDG_CONFIG_HOME ] && XDG_CONFIG_HOME="~/.config"
-[ ! -d $XDG_CONFIG_HOME ] && mkdir -p $XDG_CONFIG_HOME
+[ -z $XDG_CONFIG_HOME ] && XDG_CONFIG_HOME="$HOME/.config"
+[ ! -d $XDG_CONFIG_HOME ] && mkdir -p -- $XDG_CONFIG_HOME
 # Create symlink
 [ -L $XDG_CONFIG_HOME/nvim ] && rm $XDG_CONFIG_HOME/nvim
 ln -s $HOME/.dotfiles/neovim/nvim $XDG_CONFIG_HOME/nvim
 
 # Install Vim-Plug to default vim rum
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+echo "Install Vim-Plug manager"
+ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
