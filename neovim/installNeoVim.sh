70d2788b5b48b6f669693ab5e785c7aeea0b5400
daizeng1984 <daizeng1984@gmail.com>
2016-12-18 13:31:59 +0800

    - Fixed minor issue that install script cannot update symlink - Fixed brew is not visible for checkAndBrew function

--- neovim/installNeoVim.sh
+++ neovim/installNeoVim.sh
@@ -2,6 +2,7 @@
 [ -z $XDG_CONFIG_HOME ] && XDG_CONFIG_HOME="~/.config"
 [ ! -d $XDG_CONFIG_HOME ] && mkdir -p $XDG_CONFIG_HOME
 # Create symlink
+[ -L $XDG_CONFIG_HOME/nvim ] && rm $XDG_CONFIG_HOME/nvim
 ln -s $HOME/.dotfiles/neovim/nvim $XDG_CONFIG_HOME/nvim
 
 # Install Vim-Plug to default vim rum
