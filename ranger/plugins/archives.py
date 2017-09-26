9fd5fb19fddeff9f762dcf07daac5c1351d536ed
daizeng1984 <daizeng1984@gmail.com>
2017-09-26 21:27:53 +0800

    Added bulkrename for the ranger, happier in vim's way

--- ranger/plugins/archives.py
+++ ranger/plugins/archives.py
@@ -195,3 +195,49 @@ class copyfilepath(Command):
             linux_copy(" ".join([os.path.abspath(f.path) for f in marked_files]))
         else:
             donothing_copy(" ".join([os.path.abspath(f.path) for f in marked_files]))
+
+# Modified from https://lists.nongnu.org/archive/html/ranger-users/2010-09/msg00003.html
+class bulkrename(Command):
+  """
+  :bulkrename
+
+  This command opens a list of selected files in an external editor.
+  After you edit and save the file, it will generate a shell script
+  which does bulk renaming according to the changes you did in the file.
+
+  This shell script is opened in an editor for you to review.
+  After you close it, it will be executed.
+  """
+  def execute(self):
+    import tempfile
+    from ranger.ext.shell_escape import shell_escape as esc
+
+    # Create and edit the file list
+    cwd = self.fm.thisdir
+    
+    filenames = [f.basename for f in cwd.get_selection()]
+    listfile = tempfile.NamedTemporaryFile()
+    listfile.write("\n".join(filenames))
+    listfile.flush()
+    listfile_name = listfile.name
+    self.fm.run(['nvim', listfile_name])
+    # Interestingly, nvim is using backupcopy way to write file and we need a fresh read for the tempfile
+    # see https://stackoverflow.com/questions/22976454/vim-cannot-save-to-temporary-files-created-by-python
+    new_filenames = open(listfile_name).read().split("\n")
+    listfile.close()
+    
+    if all(a == b for a, b in zip(filenames, new_filenames)):
+      self.fm.notify("No renaming to be done!")
+      return
+
+    # Generate and execute script
+    cmdfile = tempfile.NamedTemporaryFile()
+    cmdfile.write("# This file will be executed when you close the editor.\n")
+    cmdfile.write("# Please double-check everything, clear the file to abort.\n")
+    cmdfile.write("\n".join("mv -vi " + esc(old) + " " + esc(new) \
+        for old, new in zip(filenames, new_filenames) if old != new))
+    cmdfile.flush()
+    self.fm.run(['nvim', cmdfile.name])
+    cmdfile_name = cmdfile.name
+    self.fm.run(['/bin/sh', cmdfile_name], flags='w')
+    cmdfile.close()
