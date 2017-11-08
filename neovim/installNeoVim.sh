fc88dbbe3d7a20ccb481dc5788ad3baba5c9c0cd
daizeng1984 <daizeng1984@gmail.com>
2017-11-07 21:52:24 -0800

    Fixed a bug that installNeoVim.sh cannot be created if the XDG_CONFIG folder exists

--- neovim/installNeoVim.sh
+++ neovim/installNeoVim.sh
@@ -2,6 +2,7 @@
 [ -z $XDG_CONFIG_HOME ] && XDG_CONFIG_HOME="$HOME/.config"
 [ ! -d $XDG_CONFIG_HOME ] && mkdir -p -- $XDG_CONFIG_HOME
 # Create symlink
+[ -d $XDG_CONFIG_HOME/nvim ] && rm -rf -- $XDG_CONFIG_HOME/nvim
 [ -L $XDG_CONFIG_HOME/nvim ] && rm $XDG_CONFIG_HOME/nvim
 ln -s $HOME/.dotfiles/neovim/nvim $XDG_CONFIG_HOME/nvim
 
