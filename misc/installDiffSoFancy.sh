c1e606ef147e11c64b9e2e0559ed6de8c95776b2
daizeng1984 <daizeng1984@gmail.com>
2017-11-13 22:30:46 -0800

    Added diff-so-fancy installer script

---
+++ misc/installDiffSoFancy.sh
@@ -0,0 +1,3 @@
+# Clone and symlink if not available in brew
+git clone https://github.com/so-fancy/diff-so-fancy.git $HOME/.dotfiles/misc/diff-so-fancy
+ln -s $HOME/.dotfiles/misc/diff-so-fancy/diff-so-fancy /usr/local/bin/diff-so-fancy
