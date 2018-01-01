85f52e97177c10b5361a60de0f4ec4e2e0292285
daizeng1984 <daizeng1984@gmail.com>
2017-12-31 19:27:07 -0800

    Added fzf find file and z command in ranger

--- ranger/plugins/archives.py
+++ ranger/plugins/archives.py
@@ -241,3 +241,55 @@ class bulkrename(Command):
     cmdfile_name = cmdfile.name
     self.fm.run(['/bin/sh', cmdfile_name], flags='w')
     cmdfile.close()
+
+
+# https://github.com/ranger/ranger/wiki/Commands#visit-frequently-used-directories
+# Slightly adjust its flavor
+class z(Command):
+    """
+    :z
+
+    Jump to directory using fzf and fasd
+    """
+    def execute(self):
+        import subprocess
+        arg = self.rest(1)
+        command = 'fasd -Rdl \'' + arg + '\' | fzf -1 -0 --no-sort +m'
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+
+
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
