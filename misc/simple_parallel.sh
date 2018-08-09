9f5508e726155d23eeccc9f7694246123b255e74
daizeng1984 <daizeng1984@gmail.com>
2018-08-08 23:24:01 -0700

    Improved simple_parallel.sh to be a really usable script

--- misc/simple_parallel.sh
+++ misc/simple_parallel.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-TMPDIR='./tmp/'
-declare -a cmd=(
-"cmd1" 
-"cmd2" 
-"cmd3" 
-)
+# Inspired by npm's concurent script, mainly use for easier development
+# Example: 
+#   ./simple_parallel.sh "redis-server= && redis
+#           flask= && python app.py
+#           somewatcher= && python nightwatcher.py
+
+echo $TMPDIR
+if [ "$TMPDIR" = "" ]; then
+    TMPDIR='/tmp/'
+fi
+[ ! -d $TMPDIR ] && mkdir -p $TMPDIR
+echo "Dump log to tmpdir: $TMPDIR"
+
+IFS=$'\n' read -d '' -r -a cmd << EOF
+$@
+EOF
 
 pgid=$$
 
@@ -13,16 +23,14 @@ for i in "${!cmd[@]}"; do
     c="${cmd[$i]}"
     cmdname=$(echo ${c} | awk '{print $1;}' | tr '[a-z]' '[A-Z]')
     tmplog="${TMPDIR}/${cmdname}.log"
-
     touch "$tmplog"
     esc=$(printf '\e')
+    echo "Executing command: $c"
     title="$esc[0;$(($i+41))m[${cmdname}]$esc[0m"
-    common="$c 2>&1 | tee ${tmplog}" 
     eval "$c " 2>&1 | tee "${tmplog}" 2>&1 | sed -e "s/^/${title}  /" & p=$!
     PID_LIST+="$p "
     #
 done
-
 killall() {
 for pid in $1; do
 for child in $(ps -o pid,pgid -ax | awk "{ if ( \$2 == $pid ) { print \$1 }}"); do
@@ -35,5 +43,3 @@ done
 }
 trap "killall $pgid" SIGINT
 wait $PID_LIST
-
-
