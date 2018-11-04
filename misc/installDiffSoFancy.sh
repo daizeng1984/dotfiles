b366183000ef634df29b8b8d3e101223321a91d9
daizeng1984 <daizeng1984@gmail.com>
2018-11-04 16:20:25 +0800

    Cleaned useless script

--- misc/installDiffSoFancy.sh
+++
@@ -1,3 +0,0 @@
-# Clone and symlink if not available in brew
-git clone https://github.com/so-fancy/diff-so-fancy.git $HOME/.dotfiles/misc/diff-so-fancy
-ln -s $HOME/.dotfiles/misc/diff-so-fancy/diff-so-fancy /usr/local/bin/diff-so-fancy
