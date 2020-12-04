9078233fd879bbb1f7dad7113b1ab3f3e67d2cc9
daizeng1984 <daizeng1984@gmail.com>
2020-12-04 02:27:39 -0800

    Added proot for linux Cleaned conda install

--- installConda.sh
+++ installConda.sh
@@ -2,50 +2,39 @@
 # Install miniconda
 source ./conda/installMiniconda.sh
 
-# Now conda charge!
-# Basic build setup
-conda install -y -c conda-forge cmake
-conda install -y -c conda-forge clangdev
-
-# pandoc
-conda install -y -c conda-forge pandoc
-# Suggest install system dependent (brew install gpg or yum install gnupg)
-# conda install -y -c conda-forge gnupg
-
-# Vim
-conda install -y -c conda-forge neovim
-conda install -y -c daizeng1984 nvim
-
+# Now we do minimal setup
 # Nodejs
-conda install -y -c conda-forge nodejs
+# conda install -y -c conda-forge nodejs
+# conda install -y -c conda-forge yarn
 
 # Boostrap java
-conda install -y -c daizeng1984 sdkman
+# conda install -y -c daizeng1984 sdkman
 
 # C++
-conda install -y -c conda-forge conan
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conda install -y -c daizeng1984 ccls
+# conda install -y -c conda-forge cmake
+# conda install -y -c conda-forge clangdev
+# conda install -y -c conda-forge conan
+# conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+# conda install -y -c daizeng1984 ccls
 
 # ruby
 # native lib via bundler always requires lib path from conda, not friendly at all
 # has to use system ruby
 # conda install -y -c conda-forge ruby
 
-# Web tools
-conda install -y -c conda-forge httpie
-
-# Tmux
+# Tools
+conda install -y -c conda-forge git
+conda install -y -c conda-forge zsh
+conda install -y -c conda-forge neovim
+conda install -y -c daizeng1984 nvim
 conda install -y -c conda-forge tmux
 # Fix https://github.com/conda-forge/tmux-feedstock/issues/12
 conda install -y -c conda-forge ncurses
-
 # Misc
 conda install -y -c conda-forge fzf
 # TODO: fzf recipe is not complete
 mkdir -p $HOME/.dotfiles/.local/lib/miniconda/share/fzf/plugin
 wget https://raw.githubusercontent.com/junegunn/fzf/master/plugin/fzf.vim -P $HOME/.dotfiles/.local/lib/miniconda/share/fzf/plugin
-
 conda install -y -c conda-forge rsync
 conda install -y -c conda-forge trash-cli
 conda install -y -c conda-forge ripgrep
@@ -55,16 +44,17 @@ conda install -y -c daizeng1984 fasd
 conda install -y -c conda-forge fd-find
 conda install -y -c bioconda grep # make sure we have organic grep on mac
 conda install -y -c conda-forge patool
+conda install -y -c conda-forge ranger-fm
+conda install -y -c conda-forge exa
+conda install -y -c conda-forge bat
+conda install -y -c conda-forge broot
+conda install -y -c conda-forge httpie
+conda install -y -c conda-forge htop
+conda install -y -c conda-forge pandoc
 
-# TODO: separate Non-conda
-# Vim/tmux
+# Configs
 source $HOME/.dotfiles/neovim/configNeovim.sh
-source $HOME/.dotfiles/tmux/configTmux.sh
-# TODO: anaconda ranger
-pip install ranger-fm
-# TODO: backup 
-# autocomplete ignore case for bash
-# echo "set completion-ignore-case On" >> $HOME/.inputrc
 
-# Install zsh (requires git TODO)
+source $HOME/.dotfiles/tmux/configTmux.sh
+
 git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
