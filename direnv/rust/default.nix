efc4b1ea69e812e60bef3609d55596ee7087be1c
daizeng1984 <daizeng1984@gmail.com>
2020-12-29 22:30:24 -0800

    Added coc rlsp

--- direnv/rust/default.nix
+++ direnv/rust/default.nix
@@ -1,7 +1,7 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
+  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "clippy-preview" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
 in
 with nixpkgs;
 mkShell {
@@ -10,6 +10,7 @@ mkShell {
     pkg-config
     nasm
     ruststable
+    rustup
     cmake
     zlib 
   ];
