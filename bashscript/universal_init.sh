ce1926da88e0ea1cfcf194920ddb0697d7545606
daizeng1984 <daizeng1984@gmail.com>
2022-02-11 01:08:02 -0800

    Added git prompot for zsh/bash

--- bashscript/universal_init.sh
+++ bashscript/universal_init.sh
@@ -22,10 +22,16 @@ if [ -n "$force_color_prompt" ]; then
    fi
 fi
 
+
+# git branch
+parse_git_branch() {
+     git branch --no-color 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1/"
+}
+
 if [ "$color_prompt" = yes ]; then
-   PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\[\033[00m\]\[\033[01;34m\]\w \[\033[01;31m\]$(parse_git_branch)\[\033[00m\]> '
 else
-   PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\u \w$(parse_git_branch)> '
 fi
 unset color_prompt force_color_prompt
 
--- misc/oldstyle.zsh-theme
+++ misc/oldstyle.zsh-theme
@@ -1,3 +1,3 @@
 # -*- sh -*- vim:set ft=sh ai et sw=4 sts=4:
-PROMPT='${__VIM_MODE__}%{${FG[045]}%}%B%2~ %{$FG[166]%}$( git branch --no-color 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1/" )%{$reset_color%}%(!.#.>)%b '
+PROMPT='${__VIM_MODE__}%{${FG[045]}%}%B%2~ %{$FG[166]%}$(parse_git_branch)%{$reset_color%}%(!.#.>)%b '
 RPROMPT='${__VIM_MODE__}%{$fg_bold[green]%}%D{%Y/%m/%f}|%*%{$reset_color%}'
