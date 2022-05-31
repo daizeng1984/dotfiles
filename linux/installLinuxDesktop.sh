f8fd93203a35ba6296501b59984f06111abbffda
daizeng1984 <daizeng1984@gmail.com>
2022-05-30 20:34:38 -0700

    Added linux mac-like keybindings through keyd

--- linux/installLinuxDesktop.sh
+++ linux/installLinuxDesktop.sh
@@ -1,22 +1,36 @@
-# gnome (TODO: nix it)
-# night light https://askubuntu.com/questions/1088650/gnome-night-light-setting-from-the-command-line
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+# # gnome (TODO: nix it)
+# # night light https://askubuntu.com/questions/1088650/gnome-night-light-setting-from-the-command-line
+# gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 
-# disable hot corner!
-gsettings set org.gnome.desktop.interface enable-hot-corners false
+# # disable hot corner!
+# gsettings set org.gnome.desktop.interface enable-hot-corners false
 
-# urxvt
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'urxvt'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'urxvt -e zsh'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Primary><Alt>t'
+# # urxvt
+# gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'urxvt'
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'urxvt -e zsh'
+# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Primary><Alt>t'
 
-# set the ctrl+alt+d
-gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Primary><Alt>d']"
+# # set the ctrl+alt+d
+# gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Primary><Alt>d']"
 
-# input method
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Super>space']"
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Super><Shift>space']"
+# # input method
+# gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Super>space']"
+# gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Super><Shift>space']"
 
-# generate autokey scripts
-python3 $HOME/.dotfiles/linux/configAutokey.py
+# # generate autokey scripts
+# python3 $HOME/.dotfiles/linux/configAutokey.py
+
+# Install keyd related
+_PWD=$(pwd)
+cd /tmp/
+rm -rf /tmp/keyd
+git clone https://github.com/rvaiya/keyd
+cd keyd
+git checkout v2.4.0
+sudo make uninstall && make && sudo make install
+mkdir -p /etc/keyd/
+sudo cp $DOTFILES_HOME/misc/keyd.conf /etc/keyd/default.conf
+sudo systemctl enable keyd && sudo systemctl start keyd
+
+cd $_PWD
