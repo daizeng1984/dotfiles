5beaf64135a997c7fe4ed380e4a1c94d618de86e
daizeng1984 <daizeng1984@gmail.com>
2024-12-06 17:37:46 -0800

    Fixed annoying nvim gpg issue Removed old alacritty.yml

--- minimal.sh
+++ minimal.sh
@@ -10,7 +10,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 mkdir -p $HOME/.dotfiles/.local/lib/miniconda/share/fzf/plugin
 wget https://raw.githubusercontent.com/junegunn/fzf/master/plugin/fzf.vim -P $HOME/.dotfiles/.local/lib/miniconda/share/fzf/plugin
 
-# gnupg
-echo "max-cache-ttl 34560000\ndefault-cache-ttl 34560000" >> ~/.gnupg/gpg-agent.conf
-gpgconf --kill gpg-agent
+# gnupg (use x11 pinentry for https://github.com/jamessan/vim-gnupg/issues/32)
+echo "max-cache-ttl 34560000\ndefault-cache-ttl 34560000\npinentry-program /bin/pinentry-x11" >> ~/.gnupg/gpg-agent.conf
+gpgconf --kill gpg-agent # which automatically restart in wsl
 # gpg-agent --daemon
